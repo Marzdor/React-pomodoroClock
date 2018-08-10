93d39c13741147acf182597e3b242236f86f6e95
Max Ingersoll <marzdor@gmail.com>
2018-08-10 13:29:44 -0700

    some form of countdown working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import "./App.css";
 import Settings from "./Settings";
 import Timer from "./Timer";
@@ -9,17 +10,65 @@ class App extends Component {
     this.state = {
       sessionTime: 25,
       breakTime: 5,
-      timerRunning: false,
+      timerState: "off",
       currentTimer: "Session",
-      currentTime: 25
+      time: {},
+      seconds: 1500
     };
     this.handleSettingsClick = this.handleSettingsClick.bind(this);
+    this.handleStartStopClick = this.handleStartStopClick.bind(this);
+
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
+
+  secondsToTime(secs) {
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    if (seconds < 10) seconds = ("0" + seconds).slice(-2);
+    if (minutes < 10) minutes = ("0" + minutes).slice(-2);
+
+    let obj = {
+      m: minutes,
+      s: seconds
+    };
+    return obj;
+  }
+
+  componentDidMount() {
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+  }
+
+  startTimer() {
+    if (this.timer === 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    // Remove one second, set state so a re-render happens.
+    let seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds
+    });
+
+    // Check if we're at zero.
+    if (seconds === 0) {
+      clearInterval(this.timer);
+    }
   }
 
   handleSettingsClick(e) {
     const btn = e.target;
 
-    if (!this.state.timerRunning) {
+    if (this.state.timerState === "off") {
       if (/break/.test(btn.id)) {
         console.log(btn.innerHTML);
         btn.innerHTML === "+"
@@ -37,6 +86,18 @@ class App extends Component {
     }
   }
 
+  handleStartStopClick(e) {
+    if (this.state.currentTimer === "Session") {
+      let sec = this.state.sessionTime * 60;
+      this.setState({ seconds: sec });
+    } else if (this.state.currentTimer === "Break") {
+      let sec = this.state.sessionTime * 60;
+      this.setState({ seconds: sec });
+    }
+
+    this.startTimer();
+  }
+
   render() {
     return (
       <div id="App">
@@ -46,8 +107,11 @@ class App extends Component {
           sessionTime={this.state.sessionTime}
         />
         <Timer
-          timerText={this.state.currentTime}
+          timerMin={this.state.time.m}
+          timerSec={this.state.time.s}
           currentTimer={this.state.currentTimer}
+          timerState={this.state.timerState}
+          handleStartStopClick={this.handleStartStopClick}
         />
       </div>
     );
--- src/Timer.js
+++ src/Timer.js
@@ -4,8 +4,10 @@ const Timer = props => {
   return (
     <section>
       <h2 id="timer-label">{props.currentTimer}</h2>
-      <h3 id="time-left">{props.timerText}</h3>
-      <button id="start_stop">►❚❚</button>
+      <h3 id="time-left">{props.timerMin + ":" + props.timerSec}</h3>
+      <button id="start_stop" onClick={props.handleStartStopClick}>
+        ►❚❚
+      </button>
       <button id="reset">⟳</button>
     </section>
   );
